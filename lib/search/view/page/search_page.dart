1fb275a1fd2baac92cb202d621887092b4aa260c
egawa-dev <egawa.soft.development@gmail.com>
2025-08-30 17:47:48 +0900

    feat: 検索画面に作成した部品を配置

--- lib/search/view/page/search_page.dart
+++ lib/search/view/page/search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:git_hub_repository_search_app/search/view/widget/input_keyword_field.dart';
+import 'package:git_hub_repository_search_app/search/view/widget/search_result_list.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -7,7 +9,16 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("検索")),
-      body: Padding(padding: EdgeInsets.all(20), child: Container()),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            InputKeywordField(),
+            SizedBox(height: 20),
+            Expanded(child: SearchResultList()),
+          ],
+        ),
+      ),
     );
   }
 }
