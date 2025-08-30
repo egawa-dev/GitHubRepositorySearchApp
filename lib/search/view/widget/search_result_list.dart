c6b9701565039e0e41568c266f73050785269942
egawa-dev <egawa.soft.development@gmail.com>
2025-08-31 02:47:21 +0900

    feat: 初期状態の表示を作成

--- lib/search/view/widget/search_result_list.dart
+++ lib/search/view/widget/search_result_list.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:git_hub_repository_search_app/search/%20bloc/search_bloc.dart';
+import 'package:git_hub_repository_search_app/search/%20bloc/search_state.dart';
 import 'package:git_hub_repository_search_app/search/view/widget/search_result_tile.dart';
+import 'package:git_hub_repository_search_app/shared/model/git_hub_repo.dart';
 
 /// 検索結果リスト
 class SearchResultList extends StatelessWidget {
@@ -7,12 +11,33 @@ class SearchResultList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 3, // TODO: APIで取得した結果数に合わせること
-      itemBuilder: (context, index) {
-        // TODO: 読み込み中の表示
-        return SearchResultTile();
+    return BlocBuilder<SearchBloc, SearchState>(
+      builder: (context, state) {
+        switch (state) {
+          case SearchInitial():
+            return _initialMessage();
+          case SearchSuccess(:List<GitHubRepo> results):
+            return ListView.builder(
+              itemCount: 3, // TODO: APIで取得した結果数に合わせること
+              itemBuilder: (context, index) {
+                // TODO: 読み込み中の表示
+                return SearchResultTile();
+              },
+            );
+        }
       },
+    );
+  }
+
+  // 初期状態のメッセージ
+  Widget _initialMessage() {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Text(
+        "キーワードを入力して検索してください。",
+        maxLines: 2,
+        overflow: TextOverflow.fade,
+      ),
     );
   }
 }
