e7f727350cf70c5ac6062c2fabef90388dace6a8
egawa-dev <egawa.soft.development@gmail.com>
2025-08-31 16:32:59 +0900

    feat: 次のページを読み込む処理を作成

--- lib/search/ bloc/search_bloc.dart
+++ lib/search/ bloc/search_bloc.dart
@@ -7,9 +7,16 @@ import 'package:git_hub_repository_search_app/shared/repository/repo/repo_reposi
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
   SearchBloc({required this.repoRepository}) : super(SearchInitial()) {
     on<SearchPressedSearchButton>(_onPressedSearchButton);
+    on<SearchScrollNextPagePosition>(_onScrollNextPagePosition);
   }
 
   final RepoRepositoryInterface repoRepository;
+
+  /// 現在のページ
+  int _currentPage = 0;
+
+  /// 現在の検索キーワード
+  String _searchKeyword = "";
 
   /// 検索ボタンが押された時の処理
   Future<void> _onPressedSearchButton(
@@ -24,12 +31,88 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
     emit(SearchInitialLoading());
 
     try {
+      // NOTE: 次ページを読み込むために保存
+      _searchKeyword = event.keyword;
       final results = await repoRepository.search(keyword: event.keyword);
+      _currentPage = 1;
       emit(
         SearchSuccess(results: results.repos, hasNextPage: results.hasNextPage),
       );
     } catch (_) {
       emit(SearchFailure("検索に失敗しました。"));
+    }
+  }
+
+  Future<void> _onScrollNextPagePosition(
+    SearchScrollNextPagePosition event,
+    Emitter<SearchState> emit,
+  ) async {
+    // NOTE: 以降の処理でスマートキャストされるためにローカル変数にする必要がある
+    final beforeState = state; // 処理前の状態
+    // 検索成功状態のみ処理する
+    if (beforeState is! SearchSuccess) {
+      return;
+    }
+    // 次のページが存在する場合のみ処理する
+    if (!beforeState.hasNextPage) {
+      return;
+    }
+
+    // 追加データ読み込み状態に移行
+    emit(SearchAddLoading(beforeState.results));
+
+    try {
+      // 検索APIを実行
+      final nextResults = await repoRepository.search(
+        keyword: _searchKeyword,
+        page: _currentPage + 1,
+      );
+      // NOTE: 以降の処理でスマートキャストされるためにローカル変数にする必要がある
+      final afterState = state; // 処理後の状態
+      if (afterState is SearchAddLoading) {
+        emit(
+          SearchSuccess(
+            results: [...afterState.results, ...nextResults.repos],
+            hasNextPage: nextResults.hasNextPage,
+          ),
+        );
+      } else if (afterState is SearchSuccess) {
+        // NOTE: 基本的にありえないが、非同期処理待機後なので念の為ケアしておく
+        emit(
+          SearchSuccess(
+            results: [...afterState.results, ...nextResults.repos],
+            hasNextPage: nextResults.hasNextPage,
+          ),
+        );
+      } else {
+        // NOTE: 基本的にありえないが、非同期処理待機後なので念の為ケアしておく
+        emit(
+          SearchSuccess(
+            results: nextResults.repos,
+            hasNextPage: nextResults.hasNextPage,
+          ),
+        );
+      }
+      _currentPage += 1;
+    } catch (_) {
+      // NOTE: 以降の処理でスマートキャストされるためにローカル変数にする必要がある
+      final stateAfterError = state; // エラー後の状態
+      if (stateAfterError is SearchAddLoading) {
+        // NOTE: 次ページを読み込もうとして失敗したので、次ページはまだある
+        emit(
+          SearchSuccess(results: stateAfterError.results, hasNextPage: true),
+        );
+      } else if (stateAfterError is SearchSuccess) {
+        // NOTE: 基本的にありえないが、非同期処理待機後なので念の為ケアしておく
+        // NOTE: 次ページを読み込もうとして失敗したので、次ページはまだある
+        emit(
+          SearchSuccess(results: stateAfterError.results, hasNextPage: true),
+        );
+      } else {
+        // NOTE: 基本的にありえないが、非同期処理待機後なので念の為ケアしておく
+        // NOTE: 前回のデータがわからないので、エラー画面にする
+        emit(SearchFailure("検索に失敗しました。"));
+      }
     }
   }
 }
