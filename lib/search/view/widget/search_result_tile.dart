76f3a3c176d5c9f40389f805e76e2288667d977d
egawa-dev <egawa.soft.development@gmail.com>
2025-08-31 15:10:35 +0900

    feat: 検索結果のタイルに検索結果が反映できるように変更

--- lib/search/view/widget/search_result_tile.dart
+++ lib/search/view/widget/search_result_tile.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:git_hub_repository_search_app/shared/model/git_hub_repo.dart';
 
 /// 検索結果リストのタイル
 class SearchResultTile extends StatelessWidget {
-  const SearchResultTile({super.key});
+  const SearchResultTile({super.key, required this.repo});
+
+  final GitHubRepo repo;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text("リポジトリー名"),
+        title: Text(repo.name),
         trailing: Icon(Icons.arrow_forward_ios, size: 12),
         onTap: () {
           // TODO: 詳細画面に遷移
